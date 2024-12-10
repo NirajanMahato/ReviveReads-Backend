52bedb13d5a09e0da9d550630c67e4a4150e805b
Nirajan Mahato <123573405+NirajanMahato@users.noreply.github.com>
2024-12-10 18:20:38 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+const PORT = process.env.PORT ? process.env.PORT : 5000
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
